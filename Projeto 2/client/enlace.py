812b3324ac3d3b1d435e7cd4f0696ec32e1d0a4a
Pedro2712 <72358343+Pedro2712@users.noreply.github.com>
2022-03-03 00:17:22 -0300

    Update enlace.py

--- Projeto 2/client/enlace.py
+++ Projeto 2/client/enlace.py
@@ -10,80 +10,35 @@
 # Importa pacote de tempo
 import time
 
-# Threads
-import threading
-import enlaceTx as tx
-import aplicacao as ap
+# Interface Física
+from interfaceFisica import fisica
 
-import time
+# enlace Tx e Rx
+from enlaceRx import RX 
+from enlaceTx import TX
 
-# Class
-class RX(object):
-  
-    def __init__(self, fisica):
-        self.fisica      = fisica
-        self.buffer      = bytes(bytearray())
-        self.threadStop  = False
-        self.threadMutex = True
-        self.READLEN     = 1024
+class enlace(object):
+    
+    def __init__(self, name):
+        self.fisica      = fisica(name)
+        self.rx          = RX(self.fisica)
+        self.tx          = TX(self.fisica)
+        self.connected   = False
 
-    def thread(self): 
-        while not self.threadStop:
-            if(self.threadMutex):
-                rxTemp, nRx = self.fisica.read(self.READLEN)
-                if (nRx > 0):
-                    self.buffer += rxTemp
-                time.sleep(0.01)
+    def enable(self):
+        self.fisica.open()
+        self.rx.threadStart()
+        self.tx.threadStart()
 
-    def threadStart(self):       
-        self.thread = threading.Thread(target=self.thread, args=())
-        self.thread.start()
+    def disable(self):
+        self.rx.threadKill()
+        self.tx.threadKill()
+        time.sleep(1)
+        self.fisica.close()
 
-    def threadKill(self):
-        self.threadStop = True
-
-    def threadPause(self):
-        self.threadMutex = False
-
-    def threadResume(self):
-        self.threadMutex = True
-
-    def getIsEmpty(self):
-        if(self.getBufferLen() == 0):
-            return(True)
-        else:
-            return(False)
-
-    def getBufferLen(self):
-        return(len(self.buffer))
-
-    def getAllBuffer(self, len):
-        self.threadPause()
-        b = self.buffer[:]
-        self.clearBuffer()
-        self.threadResume()
-        return(b)
-
-    def getBuffer(self):
-        # Pausa o thread
-        self.threadPause()
-        b           = self.buffer # Salva os dados do buffer na variavel b
-        # self.clearBuffer() # Zera o buffer
-        self.threadResume()
-        return(b)
-
-    def getNData(self, size):
-        time_i= time.ctime()
-        while self.getBufferLen() < size:
-            time_f= time.ctime()
-            tempo_total= ap.calcula_tempo(time_i, time_f)
-            ap.tempo_decorrido(tempo_total)
-            if tempo_total == "00:00:10":
-                print("-" * 50)
-                print ("Time Out", "\U0001F615")
-                break
-            time.sleep(0.9)
-        return(self.getBuffer())
-
-    def clearBuffer(self):
-        self.buffer = b""
+    def sendData(self, data):
+        self.tx.sendBuffer(data)
+        
+    def getData(self, size):
+        data = self.rx.getNData(size)
+        return(data, len(data))
